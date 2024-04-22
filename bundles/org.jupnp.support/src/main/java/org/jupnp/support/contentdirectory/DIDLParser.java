openhab/jupnp

commit c26170ffbab5feea81a94a89c9a8894a5db09a9c

Parse fraction track numbers. (#231)
Allow parsing of `{trackNum}/{totalTrack}` formats.

bundles/org.jupnp.support/src/main/java/org/jupnp/support/contentdirectory/DIDLParser.java
@@ -763,7 +763,7 @@ public class DIDLParser extends SAXParser {
                             new DIDLObject.Property.UPNP.DVD_REGION_CODE(Integer.valueOf(getCharacters())));
                 } else if ("originalTrackNumber".equals(localName)) {
                     getInstance().addProperty(
-                            new DIDLObject.Property.UPNP.ORIGINAL_TRACK_NUMBER(Integer.valueOf(getCharacters())));
+                            new DIDLObject.Property.UPNP.ORIGINAL_TRACK_NUMBER(Integer.valueOf(getCharacters().split("/")[0])));
                 } else if ("userAnnotation".equals(localName)) {
                     getInstance().addProperty(new DIDLObject.Property.UPNP.USER_ANNOTATION(getCharacters()));
                 }

commit f4c304d050bef01a17272a00b5993b3cdc447be7

Fix build
Caused by #231

Signed-off-by: Wouter Born <github@maindrain.net>

bundles/org.jupnp.support/src/main/java/org/jupnp/support/contentdirectory/DIDLParser.java
@@ -762,8 +762,8 @@ public class DIDLParser extends SAXParser {
                     getInstance().addProperty(
                             new DIDLObject.Property.UPNP.DVD_REGION_CODE(Integer.valueOf(getCharacters())));
                 } else if ("originalTrackNumber".equals(localName)) {
-                    getInstance().addProperty(
-                            new DIDLObject.Property.UPNP.ORIGINAL_TRACK_NUMBER(Integer.valueOf(getCharacters().split("/")[0])));
+                    getInstance().addProperty(new DIDLObject.Property.UPNP.ORIGINAL_TRACK_NUMBER(
+                            Integer.valueOf(getCharacters().split("/")[0])));
                 } else if ("userAnnotation".equals(localName)) {
                     getInstance().addProperty(new DIDLObject.Property.UPNP.USER_ANNOTATION(getCharacters()));
                 }